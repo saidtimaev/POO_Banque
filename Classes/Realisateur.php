56cc5d4f51f64f4e9703b32e6c815428b42c388a
saidtimaev <said.timaev@stagiaire-elan.fr>
2024-01-31 11:44:44 +0100

    classe Realisateur ajoutée

---
+++ Classes/Realisateur.php
@@ -0,0 +1,28 @@
+<?php
+
+Class Realisateur extends Personne
+{
+    private array $filmsRealises;
+
+    public function __construct(string $nom, string $prenom, string $sexe, string $dateNaissance)
+    {
+        parent::__construct($nom, $prenom, $sexe, $dateNaissance);
+        $this->filmsRealises = [];
+    }
+
+    
+
+
+    public function getFilmsRealises(): array
+    {
+        return $this->filmsRealises;
+    }
+
+ 
+    public function setFilmsRealises($filmsRealises)
+    {
+        $this->filmsRealises = $filmsRealises;
+
+        return $this;
+    }
+}
